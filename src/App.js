7d2bc041f0d4067cf92e79045c42c9043d3915aa
Tiffaney bolich <tiffaneykrystal@gmail.com>
2021-02-04 15:28:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Game from "./components/game/Game";
+import "./App.css";
+import image1 from "./assets/images/detroit.jpg";
+import image2 from "./assets/images/15017026_e7529dc74b_q.jpg";
+import image3 from "./assets/images/raven.jpg";
+import image4 from "./assets/images/bear.jpg";
 
-function App() {
+
+
+
+
+
+function App(props) {
+  const raccoons = {
+    name: 'Detroit Punishers',
+    logoSrc: image1,
+  };
+
+  const squirrels = {
+    name: 'Battle Creek Beavers',
+    logoSrc: image2,
+  };
+
+  const bunnies = {
+    name: 'Canton Ravens',
+    logoSrc: image3,
+  };
+
+  const hounds = {
+    name: 'Brownstown Bears',
+    logoSrc: image4,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={bunnies}
+        visitingTeam={hounds}
+      />
     </div>
-  );
+  )
 }
-
 export default App;
